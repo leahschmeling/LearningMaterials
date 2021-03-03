4dbb440b51fadcfe894c3e62b2ad715fdc09565b
leahschmeling <64282495+leahschmeling@users.noreply.github.com>
2021-03-03 11:06:55 -0600

    Create ArrayExample1.js

---
+++ JavaScript/ArrayExample1.js
@@ -0,0 +1,13 @@
+// declare array
+const numList1 = [1, 4, 5, 6, 2, 3, 1, 7, 6, 8];
+
+// initialize 'sum' variable
+let sum = 0;
+
+// iterate through array and return sum of elements
+for (let i = 0; i < numList1.length; i++){
+  sum = sum + numList1[i];
+}
+// print sum to console
+console.log("sum: " + sum);
+
