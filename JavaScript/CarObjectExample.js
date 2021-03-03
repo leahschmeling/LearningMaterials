0e2595b1067f4f7133dbfe8554f208109560c01b
leahschmeling <64282495+leahschmeling@users.noreply.github.com>
2021-03-03 11:39:29 -0600

    Update CarObjectExample.js

--- JavaScript/CarObjectExample.js
+++ JavaScript/CarObjectExample.js
@@ -21,3 +21,31 @@ console.log('Model: ' + carObject[model])
 //use dot notation to set property values
 carObject.mileage = 111000;
 console.log(carObject);
+
+
+// In addition to static properties, an object can also contain functions we refer to as methods
+const carObject = {
+  make: 'Jeep',
+  model: 'Wrangler',
+  mileage: 110000,
+  hasFuel: true,
+  
+  drive: function(miles) {
+    // Inside of an object's method, "this" literally means "this object"
+    if (this.hasFuel === true) {
+      this.mileage += miles;
+      this.hasFuel = false;
+      console.log(`${this.make} ${this.model} has driven ${miles} miles! Total mileage is now ${this.mileage}!`);
+    } else {
+      console.log(`${this.make} ${this.model} has no fuel! Fill up the tank before driving!`);
+    }
+  },
+  
+  fillUpTank: function() {
+    this.hasFuel = true;
+    console.log(`Filling up on gas... ${this.make} ${this.model} now has a full tank!`);
+  }
+};
+carObject.drive(500);
+carObject.drive(500);
+carObject.fillUpTank();
