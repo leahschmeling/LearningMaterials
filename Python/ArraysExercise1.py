5161a150708cb6f15b4167d3c6443b6ff4e0f838
leahschmeling <64282495+leahschmeling@users.noreply.github.com>
2021-03-05 09:58:44 -0600

    Create ArraysExercise1.py

---
+++ Python/ArraysExercise1.py
@@ -0,0 +1,18 @@
+# In this exercise, you will need to add numbers and strings to the correct lists using the "append" list method. You must add the numbers 1,2, and 3 to the "numbers" list, and the words 'hello' and 'world' to the strings variable.
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+strings.append("hello")
+strings.append("world")
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
